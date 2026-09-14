e05dde1e82126b7a543c6fcaa989a12fb37247c9
EmilianoPadilla <emilianopadillarobles@gmail.com>
2026-09-14 16:03:21 -0600

    new images for 3 exercises

--- frontend/src/pages/Routine.tsx
+++ frontend/src/pages/Routine.tsx
@@ -34,15 +34,6 @@ function DragHandleIcon() {
   );
 }
 
-function CameraIcon() {
-  return (
-    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-      <circle cx="12" cy="13" r="4" />
-    </svg>
-  );
-}
-
 function ExerciseRow({
   ex,
   onRemove,
@@ -93,9 +84,6 @@ function ExerciseRow({
           ) : (
             ex.name[0]?.toUpperCase()
           )}
-          <span className="absolute bottom-0 right-0 bg-charcoal/80 rounded-tl-md p-0.5 text-chalkdim">
-            <CameraIcon />
-          </span>
           <input type="file" accept="image/*" onChange={handlePhotoSelect} className="hidden" />
         </label>
         <MarqueeText text={ex.name} className="flex-1 min-w-0" />
