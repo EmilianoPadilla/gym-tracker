c1c0036cc79bb329cc3d7d2d966cdda8c1b4a3b9
EmilianoPadilla <emilianopadillarobles@gmail.com>
2026-09-14 16:56:00 -0600

    changed bottom text

--- frontend/src/pages/Routine.tsx
+++ frontend/src/pages/Routine.tsx
@@ -335,7 +335,7 @@ export default function Routine() {
           )}
 
           <p className="text-xs text-chalkdim text-center mt-8">
-            Exercise data by{" "}
+            Pictures by{" "}
             <a href="https://repdb.co" target="_blank" rel="noreferrer" className="underline">
               RepDB
             </a>
